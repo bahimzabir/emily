4886c96118f97d4561ea99048864c9f99c4b852a
bahimzabir <bahimzabir@gmail.com>
2023-09-02 00:53:36 +0100

    starting

---
+++ App.js
@@ -0,0 +1,34 @@
+import * as SplashScreen from 'expo-splash-screen'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import { useFonts } from 'expo-font'
+import { useCallback } from 'react'
+import { FONTS } from './constants/fonts'
+import AppNavigation from './navigations/AppNavigation'
+import { ThemeProvider } from './themes/ThemeProvider'
+
+SplashScreen.preventAutoHideAsync()
+
+export default function App() {
+    const [fontsLoaded] = useFonts(FONTS)
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync()
+        }
+    }, [fontsLoaded])
+
+    if (!fontsLoaded) {
+        return null
+    }
+
+    return (
+        
+        <ThemeProvider>
+                {/* //<SafeAreaView > */}
+                <SafeAreaProvider onLayout={onLayoutRootView}>
+                    <AppNavigation />
+                </SafeAreaProvider>
+       {/* // </SafeAreaView> */}
+            </ThemeProvider>
+    )
+}
